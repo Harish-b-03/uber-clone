1f1e6a3b80220b386b8b39938eac016f14039ff2
Harish-b-03 <harishbalaji369@gmail.com>
2023-08-24 12:11:13 +0530

    google-places-auto-complete added

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,11 +1,54 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import tw from "twrnc";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setStartLocation } from "../slices/nav-slice";
 
 const Home = () => {
+	const dispatch = useDispatch();
+
 	return (
-		<SafeAreaView style={tw`bg-white w-full h-full flex justify-center items-center`}>
-			<Text style={tw``}>Home</Text>
+		<SafeAreaView
+			style={tw`bg-white w-full h-full flex justify-center items-center`}
+		>
+			<Text style={tw`bg-white`}>Home</Text>
+			<View style={tw`w-full`}>
+				<GooglePlacesAutocomplete
+					placeholder="Pickup location"
+					styles={{
+						container: {
+							flex: 0,
+						},
+						textInput: {
+							fontSize: 18,
+						},
+					}}
+					fetchDetails={true}
+					onPress={(data, details = null) => {
+						dispatch(
+							setStartLocation({
+								location:
+									details !== null
+										? details.geometry.location
+										: null,
+								description: data.description,
+							})
+						);
+
+						dispatch(setDestination(null));
+					}}
+					query={{
+						key: GOOGLE_MAPS_API_KEY,
+						language: "en",
+					}}
+					minLength={2}
+					enablePoweredByContainer={false}
+					nearbyPlacesAPI="GooglePlacesSearch"
+					debounce={400}
+				/>
+			</View>
 		</SafeAreaView>
 	);
 };
